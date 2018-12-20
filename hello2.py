29bda083929206cccdfcc6889d520bbb5098226e
王亮 <redhat8@163.com>
2018-12-20 15:36:06 +0800

    app.route

--- hello2.py
+++ hello2.py
@@ -1,9 +1,33 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, render_template
+from model import User
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hello world!'
 
+@app.route('/user/<id>', methods=['POST'])
+def user_id(id):
+    return 'hello' + id
+
+@app.route('/query_user')
+def query_user():
+        id=request.args.get('id')
+        return 'query user: ' + id
+
+@app.route('/user')
+def user():
+    return render_template("hello2.html", user="peterhello2")
+
+@app.route('/object')
+def object():
+    user1 = User(1,'thinkchina')
+    return render_template("object.html", user=user1)
+
+@app.route('/query_url')
+def query_url():
+    return 'query_url is : ' + url_for('index')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ model.py
@@ -0,0 +1,4 @@
+class User(object):
+    def __init__(self, userid, user_name):
+        self.id = userid
+        self.name = user_name
