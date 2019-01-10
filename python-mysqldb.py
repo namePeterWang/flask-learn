514e8ce05adb2b565be18117c320f63792ee668c
root <root@shangtang.wwx-xxx.ali.prod.abcdefghijklmnopqrstuvw-xyz1>
2019-01-11 00:53:50 +0800

    add python-mysqldb.py

--- python-mysqldb.py
+++ python-mysqldb.py
@@ -1,1 +1,20 @@
-test
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'addcp.cn'
+app.config['MYSQL_USER'] = 'peter'
+app.config['MYSQL_PASSWORD'] = 'thinkchina'
+app.config['MYSQL_DB'] = 'thinkchina'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def users():
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT user, password FROM user''')
+	rv = cur.fetchall()
+	return str(rv)
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
